2c7b23954a596a0e7d30fff47e0b03b01820b44c
MARIANA MONTENEGRO <mariana.montenegro77@gmail.com>
2025-01-20 18:13:37 -0300

    Update commonSetup.java

--- src/test/java/Common/commonSetup.java
+++ src/test/java/Common/commonSetup.java
@@ -2,6 +2,7 @@ package Common;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
